2e6258218062b3c2cc7fbf60ea2bd4cbc7910808
KrivosheyYuriy <lonespy@yandex.ru>
2025-05-27 19:38:22 +0300

    second

--- src/main/java/org/example/webbproj/conf/FlywayServletContextListener.java
+++ src/main/java/org/example/webbproj/conf/FlywayServletContextListener.java
@@ -12,6 +12,7 @@ public class FlywayServletContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
 
         String url = System.getenv("DB_URL") == null ? "jdbc:postgresql://localhost:5432/webbproj" : System.getenv("DB_URL");
+        System.out.println(System.getenv("DB_URL"));
         String user = "postgres";
         String password = "123";
 
